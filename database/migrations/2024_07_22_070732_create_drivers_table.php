f5bcad1bd78c13742738ca7ce0b1e14be36ca3f1
Muhammad Raza. Bangi <raza.bangi@callcenter.com>
2024-07-22 12:20:03 +0500

    create crud for driver

---
+++ database/migrations/2024_07_22_070732_create_drivers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    private $tableName = 'drivers';
+
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create($this->tableName, function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 150);
+            $table->string('email', 150);
+            $table->string('phone', 150);
+            $table->boolean('status')->default(1);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists($this->tableName);
+    }
+};
